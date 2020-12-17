2b0a93a1c5aec31c0d56404bd8c2355f0f412e1d
trinibucc <tbucciarelli@hotmail.com>
2020-12-17 14:43:03 -0300

    agrego clase Posicion y Camino

---
+++ src/main/java/edu/fiuba/algo3/modelo/Camino.java
@@ -0,0 +1,13 @@
+package edu.fiuba.algo3.modelo;
+
+public class Camino {
+    private Posicion inicio;
+    private Posicion fin;
+
+    public void Camino(Posicion inicial){
+        inicio = inicial;
+        fin = new Posicion();
+    }
+
+
+}
