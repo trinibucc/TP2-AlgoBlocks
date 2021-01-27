d09f7525fc82a300d02bc9ca12439dfe70cb93f0
Martin Schipani <mschipani@fi.uba.ar>
2021-01-27 20:38:59 -0300

    Agregue pruebas en la clase Personaje.

--- src/main/test/edu/fiuba/algo3/modelo/PersonajeTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/PersonajeTest.java
@@ -1,17 +1,55 @@
 package edu.fiuba.algo3.modelo;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class PersonajeTest {
 
-    @Test
+    @Test // Prueba propuesta por la catedra.
     public void testSeCreaPersonajeConElLapizArriba() {
         Personaje personaje = new Personaje();
         assertTrue(personaje.obtenerLapiz() instanceof LapizArriba);
     }
 
+    @Test
+    public void testBajarLapizDelPersonaje(){
+        Personaje personaje = new Personaje();
+        personaje.bajarLapiz();
+        assertTrue(personaje.obtenerLapiz() instanceof  LapizAbajo);
+    }
+
+    @Test
+    public void testBajarLapizDelPersonajeYVolverASubirlo(){
+        Personaje personaje = new Personaje();
+        personaje.bajarLapiz();
+        personaje.subirLapiz();
+        assertTrue(personaje.obtenerLapiz() instanceof LapizArriba);
+    }
+
+    @Test
+    public void testPersonajeSeCreaConPosicionVertical0(){
+        Personaje personaje = new Personaje();
+        assertEquals(personaje.obtenerVertical(), 0);
+    }
+
+    @Test
+    public void testPersonajeSeCreaConPosicionHorizontal0(){
+        Personaje personaje = new Personaje();
+        assertEquals(personaje.obtenerHorizontal(), 0);
+    }
+
+    @Test
+    public void testActualizarPosicionDelPersonajeCorrectamente(){
+        Personaje personaje = new Personaje();
+        int posicionEsperada[] = {1, 2};
+        int[] posicionFinal = new int[2];
+        personaje.actualizarPosicion(1, 2);
+        posicionFinal[0] = personaje.obtenerHorizontal();
+        posicionFinal[1] = personaje.obtenerVertical();
+        assertArrayEquals(posicionFinal, posicionEsperada);
+    }
 
 
 }
