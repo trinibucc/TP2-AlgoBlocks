9a299b46b60cfe029f240a4715fcd5f9430d1ab7
trinibucc <tbucciarelli@hotmail.com>
2021-02-18 19:54:20 -0300

    feat: botone arriba, abajo, derecha

---
+++ src/main/java/edu/fiuba/algo3/MoverAbajo.java
@@ -0,0 +1,19 @@
+package edu.fiuba.algo3;
+
+import edu.fiuba.algo3.modelo.AlgoBlocks;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class MoverAbajo implements EventHandler<ActionEvent> {
+
+    private AlgoBlocks algoBlocks;
+
+    public MoverAbajo(AlgoBlocks algoBlocks){
+        this.algoBlocks = algoBlocks;
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+        algoBlocks.agregarBloqueAbajo();
+    }
+}
