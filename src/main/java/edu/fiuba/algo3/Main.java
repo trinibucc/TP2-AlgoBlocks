51ebf1fe2d65919bea8a6edb4dece3c3ea57a614
trinibucc <tbucciarelli@hotmail.com>
2021-02-23 21:20:01 -0300

    create gitignore

--- src/main/java/edu/fiuba/algo3/Main.java
+++ src/main/java/edu/fiuba/algo3/Main.java
@@ -1,13 +1,10 @@
 package edu.fiuba.algo3;
 
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
-
-
-
 
 public class Main extends Application {
 
